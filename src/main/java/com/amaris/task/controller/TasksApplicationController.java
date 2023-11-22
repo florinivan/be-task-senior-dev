c8fbb0617a4b9baf584e3fb437f96b2359252b27
florinivan <flory.stay@gmail.com>
2023-11-22 11:13:37 +0100

    crossorigin

--- src/main/java/com/amaris/task/controller/TasksApplicationController.java
+++ src/main/java/com/amaris/task/controller/TasksApplicationController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TasksApplicationController {
 
     private final TasksApplicationService taskApplicationService;
